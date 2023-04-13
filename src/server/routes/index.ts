74d56fb44e94be070ab80e98e1454af381a05160
Janes Roberto <kadoshmt2@gmail.com>
2023-04-13 14:57:40 -0400

    feat: implements pessoas controllers

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { CidadeController } from '../controllers';
+import { CidadeController, PessoasController } from '../controllers';
 
 
 
@@ -13,5 +13,11 @@ router.get('/cidades/:id', CidadeController.getByIdValidation, CidadeController.
 router.post('/cidades', CidadeController.createValidation, CidadeController.create);
 router.put('/cidades/:id', CidadeController.updateByIdValidation, CidadeController.updateById);
 router.delete('/cidades/:id', CidadeController.deleteByIdValidation, CidadeController.deleteById);
+
+router.get('/pessoas', PessoasController.getAllValidation, PessoasController.getAll);
+router.post('/pessoas', PessoasController.createValidation, PessoasController.create);
+router.get('/pessoas/:id', PessoasController.getByIdValidation, PessoasController.getById);
+router.put('/pessoas/:id', PessoasController.updateByIdValidation, PessoasController.updateById);
+router.delete('/pessoas/:id', PessoasController.deleteByIdValidation, PessoasController.deleteById);
 
 export { router };
