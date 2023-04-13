66e34b99a4fe013e8e636f2de80f5342ff73aebc
Janes Roberto <kadoshmt2@gmail.com>
2023-04-13 10:45:31 -0400

    feat: added all controllers methods to cidades

--- src/server/controllers/cidades/GetById.ts
+++ src/server/controllers/cidades/GetById.ts
@@ -2,27 +2,34 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware/Validation';
 import { StatusCodes } from 'http-status-codes';
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IParamProps {
-  id?: number; 
+  id?: number;
 }
-
-export const getByIdValidation = validation((getSchema) => ({
+export const getByIdValidation = validation(getSchema => ({
   params: getSchema<IParamProps>(yup.object().shape({
-    id: yup.number().integer().required().moreThan(0),    
-  }))
+    id: yup.number().integer().required().moreThan(0),
+  })),
 }));
 
 export const getById = async (req: Request<IParamProps>, res: Response) => {
+  if (!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: 'O parâmetro "id" precisa ser informado.'
+      }
+    });
+  }
 
-  if (Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    errors: {
-      default: 'Registro não encontrado'
-    }
-  });
+  const result = await CidadesProvider.getById(req.params.id);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
 
-  return res.status(StatusCodes.OK).json({
-    id: req.params.id,
-    nome: 'Caxias do Sul',
-  });
+  return res.status(StatusCodes.OK).json(result);
 };
--- src/server/controllers/cidades/UpdateById.ts
+++ src/server/controllers/cidades/UpdateById.ts
@@ -2,32 +2,41 @@ import { Request, Response } from 'express';
 import * as yup from 'yup';
 import { validation } from '../../shared/middleware/Validation';
 import { StatusCodes } from 'http-status-codes';
+import { ICidade } from '../../database/models';
+import { CidadesProvider } from '../../database/providers/cidades';
 
 interface IParamProps {
-  id?: number; 
+  id?: number;
 }
 
-interface ICidade {
-  nome: string;  
-}
+interface IBodyProps extends Omit<ICidade, 'id'> { }
 
-export const updateByIdValidation = validation((getSchema) => ({
-  body: getSchema<ICidade>(yup.object().shape({
-    nome: yup.string().required().min(3),    
+export const updateByIdValidation = validation(getSchema => ({
+  body: getSchema<IBodyProps>(yup.object().shape({
+    nome: yup.string().required().min(3),
   })),
   params: getSchema<IParamProps>(yup.object().shape({
-    id: yup.number().integer().required().moreThan(0),    
-  }))
+    id: yup.number().integer().required().moreThan(0),
+  })),
 }));
 
+export const updateById = async (req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
+  if (!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: 'O parâmetro "id" precisa ser informado.'
+      }
+    });
+  }
 
-export const updateById = async (req: Request<IParamProps, {}, ICidade>, res: Response) => {
+  const result = await CidadesProvider.updateById(req.params.id, req.body);
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
 
-  if (Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    errors: {
-      default: 'Registro não encontrado'
-    }
-  });
-
-  return res.status(StatusCodes.NO_CONTENT).send();
+  return res.status(StatusCodes.NO_CONTENT).json(result);
 };
