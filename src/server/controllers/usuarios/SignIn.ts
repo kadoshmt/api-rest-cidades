b16f6c89bd963da36940f0fab8fb88b97662c220
Janes Roberto <kadoshmt2@gmail.com>
2023-04-13 18:56:07 -0400

    feat: added bcryto to authenticate users

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -5,6 +5,7 @@ import * as yup from 'yup';
 import { UsuariosProvider } from '../../database/providers/usuarios';
 import { validation } from '../../shared/middleware';
 import { IUsuario } from '../../database/models';
+import { PasswordCrypto } from '../../shared/services';
 
 
 interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> { }
@@ -20,12 +21,22 @@ export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) =>
   const { email, senha } = req.body;
 
   const result = await UsuariosProvider.getByEmail(email);
-  if (result instanceof Error || senha !== result.senha) {
+
+  if (result instanceof Error) {
     return res.status(StatusCodes.UNAUTHORIZED).json({
       errors: {
         default: 'Email ou senha são inválidos'
       }
-    });  
+    });
+  }
+  const passwordMatch = await PasswordCrypto.verifyPassword(senha, result.senha);
+
+  if (!passwordMatch) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      errors: {
+        default: 'Email ou senha são inválidos'
+      }
+    });
   } else {
     return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste' });
   }
